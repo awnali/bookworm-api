b37a63448508753ea2dd20d0f8cb99a1757b3e1a
awn_lunchon <awn@turnlunchon.com>
2017-10-20 17:15:48 +0400

    auth post method added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import path from 'path'
 
 const app = express();
+app.post('/api/auth', (req,res) => {
+    setTimeout(() => res.status(400).json({errors:{global: "Invalid Credentials"}}), 3000);
+});
 app.get('/*',(req, res) => {
     res.sendFile(path.join(__dirname,'index.html'));
 });
